1312a049524bb355ebf476a55bbe6228a23a7149
Fernando Caudillo <caudillojavier@gmail.com>
2018-04-14 14:09:19 -0700

    base files for testing - VS17 working

---
+++ AugementedReal/AugementedReal/driver.cpp
@@ -0,0 +1,22 @@
+//openCV Libraries
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+//openGL Libraries
+#include <glad/glad.h> 
+#include <GLFW\glfw3.h>
+
+//standard Libraries
+#include <iostream>
+
+//namespaces
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+	cout << "openCL and openGL work!" << endl;
+
+	return 0;
+}
